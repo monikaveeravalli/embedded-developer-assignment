5e8da84a010b10d1f7ff26be8e6e8fd691432552
monikaveeravalli <96821197+monikaveeravalli@users.noreply.github.com>
2023-10-15 21:51:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,32 @@
 
 trap "kill 0" EXIT
 
-echo "Creating virtual Rx/Tx byte streams.."
+NUM_JUNIORS=5  # Specify the number of Junior instances you want to create
 
-socat -d -d pty,raw,echo=0 pty,raw,echo=0 2>&1 | grep --line-buffered "N PTY is" | grep --line-buffered -o '[^ ]*$' > socat.output &
+echo "Creating virtual Rx/Tx byte streams for 1 Senior and $NUM_JUNIORS Juniors.."
+
+# Start one Senior instance
+socat -d -d pty,raw,echo=0 pty,raw,echo=0 2>&1 | grep --line-buffered "N PTY is" | grep --line-buffered -o '[^ ]*$' > socat_output_senior &
+
+# Start multiple Junior instances
+for ((i = 1; i <= $NUM_JUNIORS; i++))
+do
+    socat -d -d pty,raw,echo=0 pty,raw,echo=0 2>&1 | grep --line-buffered "N PTY is" |  grep --line-buffered -o '[^ ]*$' > "socat_output_junior$i" &
+done
 
 sleep 2
 
-cat socat.output
+cat socat_output_senior
+TX_SENIOR=$(head -n 1 socat_output_senior)
+RX_SENIOR=$(tail -n 1 socat_output_senior)
+echo "Senior should send to $TX_SENIOR, listen on $RX_SENIOR"
 
-TX=$(head -n 1 socat.output)
-RX=$(tail -n 1 socat.output)
+# Store Tx and Rx pseudoterminals for Junior instances
+for ((i = 1; i <= NUM_JUNIORS; i++)); do
+    cat "socat_output_junior$i"
+    TX_JUNIOR[$i]=$(head -n 1 "socat_output_junior$i")
+    RX_JUNIOR[$i]=$(tail -n 1 "socat_output_junior$i")
+    echo "Junior $i should send to ${TX_JUNIOR[$i]}, listen on ${RX_JUNIOR[$i]}"
+done
 
-echo "
-Senior should send to $TX, listen on $RX
-Junior should listen on $TX, send to $RX"
 wait
